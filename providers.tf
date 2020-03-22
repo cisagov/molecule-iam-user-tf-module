62ca414a3cd3dd0012c174036fbc9820143caedd
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-21 22:44:31 -0400

    Update comment to be more accurate

--- providers.tf
+++ providers.tf
@@ -3,13 +3,13 @@
 provider "aws" {
 }
 
-# This is the provider that is used to create the policy that can
+# This is the provider that is used to create the role and policy that can
 # read Parameter Store parameters inside the Images Production account
 provider "aws" {
   alias = "images-production"
 }
 
-# This is the provider that is used to create the policy that can
+# This is the provider that is used to create the role and policy that can
 # read Parameter Store parameters inside the Images Staging account
 provider "aws" {
   alias = "images-staging"
