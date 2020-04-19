c10b4a7395a8126e99b6a78c77afe2ac7b128936
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-04-18 23:47:30 -0400

    Add a default "region" argument for the three "aws" provider declarations in providers.tf to pass `pre-commit` tests (specifically the `terraform_validate` hook).
    Per the Terraform documentation:
    region - (Optional) This is the AWS region. It must be provided, but it can also
    be sourced from the AWS_DEFAULT_REGION environment variables, or via a shared
    credentials file if profile is specified.
    
    So although these providers will be overriden by the calling module as seen in
    "examples/basic_usage/main.tf", they need to have a value set to pass a
    "terraform validate" check.

--- providers.tf
+++ providers.tf
@@ -1,16 +1,19 @@
 # This is the default provider that is used to create resources inside
 # the Users account
 provider "aws" {
+  region = "us-east-1"
 }
 
 # This is the provider that is used to create the role and policy that can
 # read Parameter Store parameters inside the Images Production account
 provider "aws" {
-  alias = "images-production"
+  region = "us-east-1"
+  alias  = "images-production"
 }
 
 # This is the provider that is used to create the role and policy that can
 # read Parameter Store parameters inside the Images Staging account
 provider "aws" {
-  alias = "images-staging"
+  region = "us-east-1"
+  alias  = "images-staging"
 }
