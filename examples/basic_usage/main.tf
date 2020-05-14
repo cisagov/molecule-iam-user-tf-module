bcc18ef44f6a89b82af8de625c1d964fcf6161f7
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-14 14:44:31 -0400

    Update example code

--- examples/basic_usage/main.tf
+++ examples/basic_usage/main.tf
@@ -8,8 +8,24 @@ provider "aws" {
 # Images (Production) account
 provider "aws" {
   region  = "us-east-1"
+  profile = "cool-images-production-provisionaccount"
+  alias   = "images-production-provisionaccount"
+}
+
+# ProvisionParameterStoreReadRoles AWS provider for the
+# Images (Staging) account
+provider "aws" {
+  region  = "us-east-1"
+  profile = "cool-images-staging-provisionaccount"
+  alias   = "images-staging-provisionaccount"
+}
+
+# ProvisionParameterStoreReadRoles AWS provider for the
+# Images (Production) account
+provider "aws" {
+  region  = "us-east-1"
   profile = "cool-images-production-provisionparameterstorereadroles"
-  alias   = "images-production"
+  alias   = "images-production-ssm"
 }
 
 # ProvisionParameterStoreReadRoles AWS provider for the
@@ -17,20 +33,22 @@ provider "aws" {
 provider "aws" {
   region  = "us-east-1"
   profile = "cool-images-staging-provisionparameterstorereadroles"
-  alias   = "images-staging"
+  alias   = "images-staging-ssm"
 }
 
 module "iam_user" {
   source = "../.."
 
   providers = {
-    aws                   = aws
-    aws.images-production = aws.images-production
-    aws.images-staging    = aws.images-staging
+    aws                                    = aws
+    aws.images-production-provisionaccount = aws.images-production-provisionaccount
+    aws.images-staging-provisionaccount    = aws.images-staging-provisionaccount
+    aws.images-production-ssm              = aws.images-production-ssm
+    aws.images-staging-ssm                 = aws.images-staging-ssm
   }
 
+  entity         = "molecule-iam-user-tf-module"
   ssm_parameters = ["/example/parameter1", "/example/config/*"]
-  user_name      = "test-molecule-iam-user-tf-module"
 
   tags = {
     Team        = "VM Fusion - Development"
