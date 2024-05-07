de8481872513557e6496b8e131322a8fc6a0788e
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-07 14:36:18 -0400

    Alphabetize

--- variables.tf
+++ variables.tf
@@ -5,13 +5,13 @@
 # ------------------------------------------------------------------------------
 
 variable "entity" {
-  type        = string
   description = "The name of the entity (usually a GitHub repository) being tested (e.g. molecule-iam-user-tf-module)."
+  type        = string
 }
 
 variable "ssm_parameters" {
-  type        = list(string)
   description = "The AWS SSM parameters that the IAM user needs to be able to read (e.g. [\"/example/parameter1\", \"/example/config/*\"])."
+  type        = list(string)
 }
 
 # ------------------------------------------------------------------------------
