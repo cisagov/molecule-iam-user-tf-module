620faaa7dae86019fe4ef5c0b9a061ce7a45c4e8
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-04 11:16:50 -0500

    Replace staging- and production-specific role outputs with a single role output

--- outputs.tf
+++ outputs.tf
@@ -4,14 +4,9 @@ output "access_key" {
   value       = module.ci_user.access_key
 }
 
-output "production_role" {
-  description = "The IAM role that the CI user can assume to read SSM parameters in the production account."
-  value       = module.ci_user.production_role
-}
-
-output "staging_role" {
-  description = "The IAM role that the CI user can assume to read SSM parameters in the staging account."
-  value       = module.ci_user.staging_role
+output "role" {
+  description = "The IAM role that the CI user can assume to read SSM parameters."
+  value       = module.ci_user.role
 }
 
 output "user" {
